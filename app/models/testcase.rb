718b18c4efeffd9c497cdfb7f219ff7fd6306f9b
kartik1982 <kartik.aiyar82@gmail.com>
2019-04-01 09:28:28 -0700

    updated test case

--- app/models/testcase.rb
+++ app/models/testcase.rb
@@ -2,7 +2,7 @@ class Testcase < ApplicationRecord
   belongs_to :testsuite
   belongs_to :user
   has_many :reports
-  validates :title, presence: true, uniqueness:true, length: {minimum: 5, maximum:50}
+  validates :title, presence: true, uniqueness:true, length: {minimum: 5, maximum:250}
   validates :description, length: {minimum: 5, maximum:100}
   validates :runmode, presence:true
 end
