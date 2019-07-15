0c6eba5c487ac0661b79fe7b927d8e0dfb250f37
kartik1982 <kartik.aiyar82@gmail.com>
2019-07-15 09:08:05 -0700

    updated changes for user add first name length restriction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save {self.email=email.downcase}
-  validates :first_name, presence: true, length: {minimum: 5, maximum:50}
+  validates :first_name, presence: true, length: {minimum: 3, maximum:50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 },
                       uniqueness: { case_sensitive: false },
