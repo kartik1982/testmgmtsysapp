b9d4c8c97c4a3f92b1c0dc3d2314e6f6f558a135
kartik1982 <kartik.aiyar82@gmail.com>
2019-09-04 08:39:40 -0700

    copy right method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,19 @@ module ApplicationHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    
     link_to title, params.permit(:release_id, :project_id, :testcycle_id).merge(sort: column, direction: direction), {class: css_class}
+  end
+
+  def copyright_generator(name, msg)
+    "Copyright &copy;#{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+  end
+
+  def sort_column
+    Report.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 end
